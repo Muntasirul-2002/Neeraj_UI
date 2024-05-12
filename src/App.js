7ec561e5997a14adbfb87e15dba2e4b414857041
muntasirul <muntasirul.msd@gmail.com>
2024-05-13 00:22:07 +0530

    ui commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Homepage from "./pages/Homepage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,142 @@
+import React from "react";
+import "../styles/product.css";
+import Navbar from "../components/Navbar";
+const Homepage = () => {
+  return (
+    <>
+      <Navbar />
+
+      <div>
+        <h1 className="d-flex justify-content-center mt-4">Our Products</h1>
+        <div class="container">
+          <div class="product-row">
+            <div class="product">
+              <div class="wrapper">
+                <div class="banner-image"></div>
+                <h1>Toyota Supra</h1>
+                <p>
+                  Lorem ipsum dolor sit amet,
+                  <br />
+                  consectetur adipiscing elit.
+                </p>
+              </div>
+              <div class="button-wrapper">
+                <button class="btn outline">DETAILS</button>
+                <button class="btn fill">BUY NOW</button>
+              </div>
+            </div>
+            <div class="product">
+              <div class="wrapper">
+                <div class="banner-image"></div>
+                <h1>Toyota Supra</h1>
+                <p>
+                  Lorem ipsum dolor sit amet,
+                  <br />
+                  consectetur adipiscing elit.
+                </p>
+              </div>
+              <div class="button-wrapper">
+                <button class="btn outline">DETAILS</button>
+                <button class="btn fill">BUY NOW</button>
+              </div>
+            </div>
+            <div class="product">
+              <div class="wrapper">
+                <div class="banner-image"></div>
+                <h1>Toyota Supra</h1>
+                <p>
+                  Lorem ipsum dolor sit amet,
+                  <br />
+                  consectetur adipiscing elit.
+                </p>
+              </div>
+              <div class="button-wrapper">
+                <button class="btn outline">DETAILS</button>
+                <button class="btn fill">BUY NOW</button>
+              </div>
+            </div>
+            <div class="product">
+              <div class="wrapper">
+                <div class="banner-image"></div>
+                <h1>Toyota Supra</h1>
+                <p>
+                  Lorem ipsum dolor sit amet,
+                  <br />
+                  consectetur adipiscing elit.
+                </p>
+              </div>
+              <div class="button-wrapper">
+                <button class="btn outline">DETAILS</button>
+                <button class="btn fill">BUY NOW</button>
+              </div>
+            </div>
+          </div>
+          <div class="product-row">
+            <div class="product">
+              <div class="wrapper">
+                <div class="banner-image"></div>
+                <h1>Toyota Supra</h1>
+                <p>
+                  Lorem ipsum dolor sit amet,
+                  <br />
+                  consectetur adipiscing elit.
+                </p>
+              </div>
+              <div class="button-wrapper">
+                <button class="btn outline">DETAILS</button>
+                <button class="btn fill">BUY NOW</button>
+              </div>
+            </div>
+            <div class="product">
+              <div class="wrapper">
+                <div class="banner-image"></div>
+                <h1>Toyota Supra</h1>
+                <p>
+                  Lorem ipsum dolor sit amet,
+                  <br />
+                  consectetur adipiscing elit.
+                </p>
+              </div>
+              <div class="button-wrapper">
+                <button class="btn outline">DETAILS</button>
+                <button class="btn fill">BUY NOW</button>
+              </div>
+            </div>
+            <div class="product">
+              <div class="wrapper">
+                <div class="banner-image"></div>
+                <h1>Toyota Supra</h1>
+                <p>
+                  Lorem ipsum dolor sit amet,
+                  <br />
+                  consectetur adipiscing elit.
+                </p>
+              </div>
+              <div class="button-wrapper">
+                <button class="btn outline">DETAILS</button>
+                <button class="btn fill">BUY NOW</button>
+              </div>
+            </div>
+            <div class="product">
+              <div class="wrapper">
+                <div class="banner-image"></div>
+                <h1>Toyota Supra</h1>
+                <p>
+                  Lorem ipsum dolor sit amet,
+                  <br />
+                  consectetur adipiscing elit.
+                </p>
+              </div>
+              <div class="button-wrapper">
+                <button class="btn outline">DETAILS</button>
+                <button class="btn fill">BUY NOW</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Homepage;
